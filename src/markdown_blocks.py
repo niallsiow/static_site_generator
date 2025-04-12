e134f6db6bb7a52ced8a6bcc4d72eeaa66391da4
nisi_pop_desktop <49096264+niallsiow@users.noreply.github.com>
2025-04-12 16:06:55 +0100

    Add function and tests for getting block type from text
    Add tests for block to blocktype function

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -7,3 +19,33 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+
+def block_to_blocktype(block):
+    lines = block.split("\n")
+    headings = ("# ", "## ", "### ", "#### ", "##### ", "###### ")
+
+    if block.startswith(headings):
+        return BlockType.HEADING
+    
+    if len(lines) > 1 and block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
